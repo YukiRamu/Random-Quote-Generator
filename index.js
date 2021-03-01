530c85e917611b2864fc30d85027dfc59ee0390d
Yuki <yuki_matsubara@outlook.com>
2021-03-01 09:20:43 -0800

    commit

--- index.js
+++ index.js
@@ -25,23 +25,19 @@ const loader = document.getElementById("loader");
 const url = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
 
 const getQuote = () => {
-  console.log("1. button was clicked");
-  let randomIndex = Math.floor(Math.random() * (authorArray.length));
+  let randomIndex = Math.floor(Math.random() * (authorArray.length)); //length = 11
+
   fetch(url)
     .then(response => response.json())//convert Response to JSON
     .then((data) => { //can access actual data
 
       /* Loading Animation */
       const loadAnimation = async function () {
-        console.log("2 loader Animation starts");
-
         loader.style.display = "block";
-        console.log(loader.style.display);
       }
 
       /* Wait 1 sec */
       const wait = async function () {
-        console.log("3 sconds waiting")
         return new Promise(function (resolve, reject) {
           setTimeout(resolve, 1000);
         });
@@ -49,21 +45,17 @@ const getQuote = () => {
 
       /* Loading Stop */
       const loadStop = async function () {
-        console.log("3. stop loader");
-        console.log(loader.style.display);
         loader.style.display = "none";
       }
 
       /* Generate Quote */
       const generateQuote = async function () {
-        console.log("3 quote generation starts");
         quoteOutput.innerHTML = `<p class="quo">" ${data.message} "</p>
         <p class="author"> - ${authorArray[randomIndex]} -</p>`; //show message and author
         header.style.transform = "translate(0%,0%)";
         header.style.top = "5%";
         header.style.left = "0%";
         output.setAttribute("class", "output clicked");
-        console.log("4 quote generation ends");
       };
 
       const processAll = async function () {
